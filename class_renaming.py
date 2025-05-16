c9d3cff513f75ab35b11aeedaa20fba6c093138f
June <166293934+juundayo@users.noreply.github.com>
2025-05-16 12:45:57 +0000

    Added capital upper case tonos classes.

--- class_renaming.py
+++ class_renaming.py
@@ -1,5 +1,11 @@
+# ----------------------------------------------------------------------------#
+
+'''
+This is the entire dataset containing 90+ classes.
+Missing the capital tonos variants for now.
+
 ORIGINAL_CLASSES = [
-    'α_cap', 'α_small', 'α_t', 'αι_cap', 'αι_small', 'αυ_cap', 'αυ_small', 'αυ_t',
+    'α_cap', 'α_cap_t', 'α_small', 'α_t', 'αι_cap', 'αι_small', 'αυ_cap', 'αυ_small', 'αυ_t',
     'β_cap', 'β_small', 'γ_cap', 'γ_small', 'γγ_cap', 'γγ_small', 'γκ_cap', 'γκ_small',
     'δ_cap', 'δ_small', 'ε_cap', 'ε_small', 'ε_t', 'ει_cap', 'ει_small', 'ει_t',
     'ευ_cap', 'ευ_small', 'ευ_t', 'ζ_cap', 'ζ_small', 'η_cap', 'η_small', 'η_t',
@@ -12,10 +18,45 @@ ORIGINAL_CLASSES = [
     'υ_διαλ', 'υ_τον_διαλ', 'υι_cap', 'υι_small', 'φ_cap', 'φ_small', 'χ_cap', 'χ_small',
     'ψ_cap', 'ψ_small', 'ω_cap', 'ω_small', 'ω_t', 'space'
 ]
+'''
+
+# ----------------------------------------------------------------------------#
+
+# Includes every possible single letter character + space. 
+ORIGINAL_CLASSES = [
+    'α_cap', 'α_cap_t', 'α_small', 'α_t', 
+    'β_cap', 'β_small',
+    'γ_cap', 'γ_small',
+    'δ_cap', 'δ_small',
+    'ε_cap', 'ε_cap_t', 'ε_small', 'ε_t', 
+    'ζ_cap', 'ζ_small', 
+    'η_cap', 'η_cap_t', 'η_small', 'η_t',
+    'θ_cap', 'θ_small', 
+    'ι_cap', 'ι_cap_t', 'ι_small', 'ι_t', 'ι_διαλ', 'ι_τον_διαλ', 
+    'κ_cap', 'κ_small',
+    'λ_cap', 'λ_small',
+    'μ_cap', 'μ_small', 
+    'ν_cap', 'ν_small',
+    'ξ_cap', 'ξ_small',
+    'ο_cap', 'ο_cap_t', 'ο_small', 'ο_t',
+    'π_cap', 'π_small',
+    'ρ_cap', 'ρ_small', 
+    'σ_2_small', 'σ_cap', 'σ_small', 
+    'τ_cap', 'τ_small',  
+    'υ_cap', 'υ_cap_t', 'υ_small', 'υ_t',
+    'υ_διαλ', 'υ_τον_διαλ',
+    'φ_cap', 'φ_small',
+    'χ_cap', 'χ_small',
+    'ψ_cap', 'ψ_small', 
+    'ω_cap', 'ω_cap_t', 'ω_small', 'ω_t', 
+    'space'
+]
+
+# ----------------------------------------------------------------------------#
 
 class_mapping = {cls: cls for cls in ORIGINAL_CLASSES}
 
-# Single letters.
+''' Single letters. '''
 class_mapping['α_cap'] = 'Α'
 class_mapping['α_small'] = 'α'
 class_mapping['β_cap'] = 'Β'
@@ -66,7 +107,9 @@ class_mapping['ψ_small'] = 'ψ'
 class_mapping['ω_cap'] = 'Ω'
 class_mapping['ω_small'] = 'ω'
 
-# Double letters.
+
+'''Double letters.'''
+'''
 class_mapping['αι_cap'] = 'ΑΙ'
 class_mapping['αι_small'] = 'αι'
 class_mapping['αυ_cap'] = 'ΑΥ'
@@ -95,24 +138,38 @@ class_mapping['τσ_cap'] = 'ΤΣ'
 class_mapping['τσ_small'] = 'τσ'
 class_mapping['υι_cap'] = 'ΥΙ'
 class_mapping['υι_small'] = 'υι'
+'''
 
-# Tonos and dialytika variants.
+
+''' Tonos and dialytika variants. '''
+# Single characters.
+class_mapping['α_cap_t'] = 'Ά'
 class_mapping['α_t'] = 'ά'
+class_mapping['ε_cap_t'] = 'Έ'
 class_mapping['ε_t'] = 'έ'
+class_mapping['η_cap_t'] = 'Ή'
 class_mapping['η_t'] = 'ή'
+class_mapping['ι_capt'] = 'Ί'
 class_mapping['ι_t'] = 'ί'
+class_mapping['ο_cap_t'] = 'Ό'
 class_mapping['ο_t'] = 'ό'
+class_mapping['υ_cap_t'] = 'Ύ'
 class_mapping['υ_t'] = 'ύ'
+class_mapping['ω_cap_t'] = 'Ώ'
 class_mapping['ω_t'] = 'ώ'
 class_mapping['ι_διαλ'] = 'ϊ'
 class_mapping['ι_τον_διαλ'] = 'ΐ'
 class_mapping['υ_διαλ'] = 'ϋ'
 class_mapping['υ_τον_διαλ'] = 'ΰ'
+
+'''
+# Double characters.
 class_mapping['αυ_t'] = 'αύ'
 class_mapping['ει_t'] = 'εί'
 class_mapping['ευ_t'] = 'εύ'
 class_mapping['οι_t'] = 'οί'
 class_mapping['ου_t'] = 'ού'
+'''
 
 # Special characters.
 class_mapping['space'] = ' '
